72732d5d6e681506b81a8e47f216bdca43a95ded
harveybia <harvey.shi@yahoo.com>
2020-04-30 23:06:54 -0400

    working OMP version

--- src/lidar_depth_renderer.cpp
+++ src/lidar_depth_renderer.cpp
@@ -30,16 +30,22 @@ void LidarDepthRenderer::render(cv::Mat &result,
 
   // erase result image
   result.setTo(0);
+#if OMP
+  // erase local image
+  for (int ii = 0; ii < omp_mats.size(); ii++)
+  {
+    omp_mats.at(ii).setTo(0);
+  }
+#endif
 
   // render points in camera_cloud
-
+  START_ACTIVITY(ACTIVITY_RENDER);
   int processed_pts = 0;
   size_t total_pts = (*cloud_ptr).size();
 #if OMP
   #pragma omp parallel firstprivate (processed_pts)
   {
   int tid = omp_get_thread_num();
-  // processed_pts = 0;
 
   #pragma omp for
 #else
@@ -58,52 +64,65 @@ void LidarDepthRenderer::render(cv::Mat &result,
 #endif
 
     // transform point to camera frame
-    START_ACTIVITY(ACTIVITY_TRANSFORM);
     const auto cam_point = transform_point(point, to_camera_tf);
-    FINISH_ACTIVITY(ACTIVITY_TRANSFORM);
 
     // discard points that are behind the camera
     if (cam_point.z <= 0.0) continue;
 
     // compute projected image coordinates
-    START_ACTIVITY(ACTIVITY_RENDER);
     const auto u = static_cast<int>((fx * cam_point.x) / cam_point.z + cx);
     const auto v = static_cast<int>((fy * cam_point.y) / cam_point.z + cy);
-    FINISH_ACTIVITY(ACTIVITY_RENDER);
-
-    START_ACTIVITY(ACTIVITY_PROJECT);
+    
     // bloat points in the image using bloat_factor
     if (u - bloat_factor >= 0 && u + bloat_factor < width &&
         v - bloat_factor >= 0 && v + bloat_factor < height) {
-      const auto cur_depth = static_cast<uint16_t>(cam_point.z * 1000.0);
+      // const auto cur_depth = static_cast<uint8_t>(cam_point.z * 1000.0);
+      const auto cur_depth = static_cast<uint8_t>(cam_point.z * 2.0);
       for (int i = -bloat_factor; i <= bloat_factor; i++) {
         for (int j = -bloat_factor; j <= bloat_factor; j++) {
           // get the previous depth
-          const auto prev_depth = local_result->at<uint16_t>(v + i, u + j);
+          const auto prev_depth = local_result->at<uint8_t>(v + i, u + j);
           // update if no previous depth or closer than previous depth
-          if (prev_depth == 0.0 || cur_depth < prev_depth) {
-            local_result->at<uint16_t>(v + i, u + j) = cur_depth;
+          if ((prev_depth == 0 || cur_depth < prev_depth) && cur_depth < 255) {
+            local_result->at<uint8_t>(v + i, u + j) = cur_depth;
           }
         }
       }
     }
-    FINISH_ACTIVITY(ACTIVITY_PROJECT);
+    
   }
 
 #if OMP
-  fprintf(stderr, "Thread %d processed %d\\%lupoints\n",
-    tid, processed_pts, (*cloud_ptr).size());
+  // fprintf(stderr, "Thread %d processed %d\\%lupoints\n",
+  //   tid, processed_pts, (*cloud_ptr).size());
   }
 #endif
 
+FINISH_ACTIVITY(ACTIVITY_RENDER);
+
+START_ACTIVITY(ACTIVITY_REDUCE);
   // Reduce serially
-// #if OMP
-//   result = omp_mats.at(0);
-//   for (cv::Mat &local_mat : omp_mats)
-//   {
-//     result = cv::min(result, local_mat);
-//   }
-// #endif
+#if OMP
+  cv::Mat f2_zero_mask;
+  cv::Mat and_mask;
+  cv::Mat temp;
+
+  // First frame
+  result = omp_mats.at(0);
+  f2_zero_mask = result == 0;
+  result.setTo(255, f2_zero_mask);
+  for (cv::Mat &local_mat : omp_mats)
+  {
+    f2_zero_mask = local_mat == 0;
+    // Set zeros to MAX to simplify min()
+    local_mat.setTo(255, f2_zero_mask);
+    cv::min(local_mat, result, result);
+  }
+  // Set MAX back to zeros for correctness
+  f2_zero_mask = result == 255;
+  result.setTo(0, f2_zero_mask);
+#endif
+FINISH_ACTIVITY(ACTIVITY_REDUCE);
 
 }
 
@@ -134,7 +153,7 @@ void LidarDepthRenderer::init(int height, int width)
   fprintf(stderr, "init mat, nthread=%d\n", n_threads);
   for (int i = 0; i < n_threads; i++)
   {
-    cv::Mat new_mat = cv::Mat::zeros(image_height, image_width, CV_16UC1);
+    cv::Mat new_mat = cv::Mat::zeros(image_height, image_width, CV_8UC1);
     omp_mats.push_back(new_mat);
   }
 #endif
